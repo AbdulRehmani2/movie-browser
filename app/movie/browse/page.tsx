990bd97ceb05fd4b99cbe1fedb9f89f45c7d9c1a
AbdulRehmani2 <abdulrehmani2238@gmail.com>
2024-08-31 10:53:35 +0500

    Fixed build errors

--- app/movie/browse/page.tsx
+++ app/movie/browse/page.tsx
@@ -2,12 +2,7 @@
 import React, { Suspense } from "react";
 import Loading from "@/components/Loading";
 import BrowseComponent from "@/components/BrowseComponent";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Browse Movies | Find all your favourite movies",
-  description: "Browse all the new and trending movies",
-};
 function Page() {
   return (
     <Suspense fallback={<Loading />}>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,12 +2,7 @@
 import React, { Suspense } from "react";
 import Loading from "@/components/Loading";
 import SearchComponent from "@/components/SearchComponent";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Search | Find all your favourite movies",
-  description: "Search your favourite movies",
-};
 function Page() {
   return (
     <Suspense fallback={<Loading />}>
