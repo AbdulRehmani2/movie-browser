94ea11303335d8789d951d1c721946183be6e32a
AbdulRehmani2 <abdulrehmani2238@gmail.com>
2024-08-31 17:35:27 +0500

    Fixed URL related errors

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -3,15 +3,15 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://${process.env.NEXT_PUBLIC_VERCEL_URL}/",
+      url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`,
       lastModified: new Date(),
     },
     {
-      url: "https://${process.env.NEXT_PUBLIC_VERCEL_URL}/search",
+      url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/search`,
       lastModified: new Date(),
     },
     {
-      url: "https://${process.env.NEXT_PUBLIC_VERCEL_URL}/movie/browse",
+      url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/movie/browse`,
       lastModified: new Date(),
     },
   ];
